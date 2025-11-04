fa86dcc2a0e03c810d6e6f696b6e5e813e0ae35a
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-11-04 22:06:58 +0100

    chore: aggiunte icone e fix per Sveltekit

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,7 @@
+// Fix for the imports:
+// https://github.com/unplugin/unplugin-icons?tab=readme-ov-file#frameworks
+import 'unplugin-icons/types/svelte'
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vite';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    Icons({
+      compiler: 'svelte'
+    })
+  ]
 });
